0f77ebb2882a84ebb4d2bfb459082185dc0f33a5
yinlong <yinlong@coolfishgames.com>
2020-06-09 13:24:38 +0800

    read me

--- psyduck_world/webserver/psyduck/urls.py
+++ psyduck_world/webserver/psyduck/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('validate', views.validate),
+    path('validate_get_state', views.validate_get_state),
+    path('login', views.login),
+    path('login_get_state', views.login_get_state),
+    path('login_get_qrcode', views.login_get_qrcode),
+    path('login_verify_get', views.login_verify_get),
+    path('login_verify_set', views.login_verify_set),
 ]
--- psyduck_world/webserver/psyduck/views.py
+++ psyduck_world/webserver/psyduck/views.py
@@ -9,16 +9,12 @@ def _get(request, key):
 
 
 def index(request):
-    token = _get(request, 'token')
     action = _get(request, 'action')
+    token = _get(request, 'token')
     uid = _get(request, 'uid')
     message = _get(request, 'message')
     json_result = ''
-    if action == 'validate':
-        json_result = action_api.validate_csdn(token, uid, message)
-    elif action == 'validate_get_state':
-        json_result = action_api.validate_get_state(token, uid, message)
-    elif action == 'login':
+    if action == 'login':
         json_result = action_api.login_get_qr_code(token, uid)
     elif action == 'login_get_qrcode':
         json_result = action_api.login_get_qr_code(token, uid)
@@ -28,4 +24,61 @@ def index(request):
         json_result = action_api.login_verify_get(token, uid, message)
     elif action == 'login_verify_set':
         json_result = action_api.login_verify_set(token, uid, message)
+    elif action == 'validate':
+        json_result = action_api.validate_csdn(token, uid, message)
+    elif action == 'validate_get_state':
+        json_result = action_api.validate_get_state(token, uid, message)
+    return HttpResponse(json_result)
+
+
+def login(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    json_result = action_api.login_get_qr_code(token, uid)
+    return HttpResponse(json_result)
+
+
+def login_get_qrcode(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    json_result = action_api.login_get_qr_code(token, uid)
+    return HttpResponse(json_result)
+
+
+def login_get_state(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    json_result = action_api.login_get_state(token, uid)
+    return HttpResponse(json_result)
+
+
+def login_verify_get(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    message = _get(request, 'message')
+    json_result = action_api.login_verify_get(token, uid, message)
+    return HttpResponse(json_result)
+
+
+def login_verify_set(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    message = _get(request, 'message')
+    json_result = action_api.login_verify_set(token, uid, message)
+    return HttpResponse(json_result)
+
+
+def validate(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    message = _get(request, 'csdn')
+    json_result = action_api.validate_csdn(token, uid, message)
+    return HttpResponse(json_result)
+
+
+def validate_get_state(request):
+    token = _get(request, 'token')
+    uid = _get(request, 'uid')
+    message = _get(request, 'csdn')
+    json_result = action_api.validate_get_state(token, uid, message)
     return HttpResponse(json_result)
