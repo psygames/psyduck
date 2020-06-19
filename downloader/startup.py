2aea127f80c5b67529495d4fca64ad39fafec503
yinlong <yinlong@coolfishgames.com>
2020-06-19 14:14:08 +0800

    remove downloader

--- downloader/startup.py
+++
@@ -1,4 +0,0 @@
-import helper
-
-helper.init()
-helper.auto_download("https://download.csdn.net/download/zzia615/4510757")
--- downloader/tools.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-def abspath(path):
-    return os.path.join(os.path.split(os.path.realpath(__file__))[0], path)
