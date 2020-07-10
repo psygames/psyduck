aa0f6c92ca1c3e3cb330c5be45dbbd982bd5a979
yinlong <yinlong@coolfishgames.com>
2020-07-10 13:25:32 +0800

    log

--- psyduck_world/core/log.py
+++ psyduck_world/core/log.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 
-def info(name, msg, desc):
+def info(name, msg, desc=None):
     _time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f'{_time} -> [{name}] {msg} ({desc})')
+    print(f'{_time} -> [{name}] {msg}')
+    if desc is not None:
+        print(f'    - {desc}')
